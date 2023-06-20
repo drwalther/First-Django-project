e7f1d3bcedad6b68522cffbab351e69d2a47255b
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-06-20 19:43:03 +0300

    added test of ordering

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -11,10 +11,10 @@ class BookApiTestCase(APITestCase):
     def setUp(self) -> None:
         self.book_1 = Book.objects.create(name='Alice', price=1000,
                                           author_name='Author 1')
-        self.book_2 = Book.objects.create(name='War and Peace', price=1200,
+        self.book_2 = Book.objects.create(name='War and Peace', price=1500,
                                           author_name='Author 3')
         self.book_3 = Book.objects.create(name='The life of Author 1',
-                                          price=1500, author_name='Author 2')
+                                          price=1200, author_name='Author 2')
 
     def test_get(self):
         url = reverse('book-list')
@@ -29,5 +29,13 @@ class BookApiTestCase(APITestCase):
         response = self.client.get(url, data={'search': 'Author 1'})
         serializer_data = BooksSerializer([self.book_1, self.book_3],
                                           many=True).data
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+        self.assertEquals(serializer_data, response.data)
+
+    def test_get_ordering(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'ordering': 'price'})
+        serializer_data = BooksSerializer([self.book_1, self.book_3,
+                                           self.book_2], many=True).data
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         self.assertEquals(serializer_data, response.data)
