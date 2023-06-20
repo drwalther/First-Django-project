7a2564e09b4484a77f2f0be3a4ca64b455a9573b
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-06-20 18:30:55 +0300

    added search

--- store/models.py
+++ store/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=250)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    author_name = models.CharField(max_length=250, default='')
 
     def __str__(self):
-        return f'{self.id}: {self.name}'
+        return f'id {self.id}: {self.name}'
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
@@ -8,5 +9,6 @@ from store.serializers import BooksSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['price']
+    search_fields = ['name', 'author_name']
