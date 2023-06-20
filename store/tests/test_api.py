173b5830e233658ae550bbba43bcea8aed1c4f32
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-06-20 19:29:38 +0300

    added test for filter

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -7,12 +7,27 @@ from store.serializers import BooksSerializer
 
 
 class BookApiTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.book_1 = Book.objects.create(name='Alice', price=1000,
+                                          author_name='Author 1')
+        self.book_2 = Book.objects.create(name='War and Peace', price=1200,
+                                          author_name='Author 3')
+        self.book_3 = Book.objects.create(name='The life of Author 1',
+                                          price=1500, author_name='Author 2')
+
     def test_get(self):
-        book_1 = Book.objects.create(name='Alice', price=1000)
-        book_2 = Book.objects.create(name='War and Peace', price=1200)
         url = reverse('book-list')
         response = self.client.get(url)
-        serializer_data = BooksSerializer([book_1, book_2], many=True).data
+        serializer_data = BooksSerializer([self.book_1, self.book_2,
+                                           self.book_3], many=True).data
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         self.assertEquals(serializer_data, response.data)
 
+    def test_get_filter(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})
+        serializer_data = BooksSerializer([self.book_1, self.book_3],
+                                          many=True).data
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+        self.assertEquals(serializer_data, response.data)
