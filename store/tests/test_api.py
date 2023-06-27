64c916df385f637330c69304ffee06fa4ab29f6b
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-06-27 16:02:48 +0300

    added test for book creation

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -8,7 +11,8 @@ from store.serializers import BooksSerializer
 
 class BookApiTestCase(APITestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
+        self.user = User.objects.create(username='test_user')
         self.book_1 = Book.objects.create(name='Alice', price=1000,
                                           author_name='Author 1')
         self.book_2 = Book.objects.create(name='War and Peace', price=1500,
@@ -39,3 +43,16 @@ class BookApiTestCase(APITestCase):
                                            self.book_2], many=True).data
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         self.assertEquals(serializer_data, response.data)
+
+    def test_create(self):
+        url = reverse('book-list')
+        data = {
+            'name': 'Black fire',
+            'price': 760,
+            'author_name': 'Jack Jacobson'
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data,
+                                    content_type='application/json')
+        self.assertEquals(status.HTTP_201_CREATED, response.status_code)
